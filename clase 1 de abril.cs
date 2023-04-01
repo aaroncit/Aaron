6d1b6835266b8ae31fa1577fe492164c86277649
Aarón Díaz <125701352+aaroncit@users.noreply.github.com>
2023-04-01 09:07:56 -0600

    Add files via upload

---
+++ clase 1 de abril.cs
@@ -0,0 +1,12 @@
+﻿void arreglos()
+{
+    String[] nombre = new String[]{
+  "Fernanda", "Melvin", "Luis", "Brando", "Yamileth"
+   };
+    int[] notas = new int[]{
+      70,90,43,85,30
+  };
+    int maxGrade = notas[0]; // Assume first grade is the highest
+    int maxIndex = 0; // Index of the highest grade
+}
+
